2f1883e543fabdb69a613a5f060619c33473f9b6
Nitish <shahi.nitish42@gmail.com>
2023-03-11 23:48:40 +0530

    Part 3: Failing test case

--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -8,7 +8,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RestaurantTest {
     Restaurant restaurant;
-    //REFACTOR ALL THE REPEATED LINES OF CODE
     @BeforeEach
     public void createRestaurant(){
         LocalTime openingTime = LocalTime.parse("10:30:00");
@@ -23,7 +22,7 @@ class RestaurantTest {
     //-------FOR THE 2 TESTS BELOW, YOU MAY USE THE CONCEPT OF MOCKING, IF YOU RUN INTO ANY TROUBLE
     @Test
     public void is_restaurant_open_should_return_true_if_time_is_between_opening_and_closing_time(){
-        //WRITE UNIT TEST CASE HERE
+
         //Arrange
         LocalTime timeBetweenOpeningAndClosingTime = LocalTime.parse("11:00:00");
         Restaurant spiedRestaurant = Mockito.spy(restaurant);
@@ -36,7 +35,6 @@ class RestaurantTest {
 
     @Test
     public void is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time(){
-        //WRITE UNIT TEST CASE HERE
         //Arrange
         LocalTime timeAfterClosingTime = LocalTime.parse("23:00:00");
         Restaurant spiedRestaurant = Mockito.spy(restaurant);
